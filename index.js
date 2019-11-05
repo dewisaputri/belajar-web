98334e69a2afca299e0e80e540a10c8d0e1f5a1b
dewisaputri <saputridew@yahoo.co.id>
2019-11-05 21:44:22 +0800

    file project web

---
+++ index.js
@@ -0,0 +1,42 @@
+
+
+const carousel = document.getElementById("carousel");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+
+const carouselItem = carousel.children;
+
+let width = 0;
+var slide = false;
+
+next.addEventListener("click", function() {
+  slide = true;
+  for (let i = 0; i < carouselItem.length; i++) {
+    const item = carouselItem[i];
+
+    if (slide) {
+      width -= item.clientWidth;
+      slide = false;
+    }
+
+    item.style.transform = `translateX(${width}px)`;
+    console.log(width);
+  }
+});
+
+prev.addEventListener("click", function() {
+  slide = true;
+  for (let i = 0; i < carouselItem.length; i++) {
+    const item = carouselItem[i];
+
+    if (slide) {
+      width += item.clientWidth;
+      slide = false;
+    }
+
+    item.style.transform = `translateX(${width}px)`;
+    console.log(width);
+  }
+});
+
+// console.log(carouselItem)
---
+++ script.js
@@ -0,0 +1,16 @@
+const nama = "aan";
+
+switch (nama) {
+    case "aan": {
+        console.log("aan")
+        break;
+}
+case "ikram": {
+    console.log("ikram")
+    break;
+}
+default: {
+    console.log("tidak yg sama")
+    break;
+    }
+}
